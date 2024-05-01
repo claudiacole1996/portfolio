243f640625d57b3138dc29c67ce37662e5fe9506
Claudia McCarter <claudiacole1996@gmail.com>
2024-04-30 20:33:05 -0400

    google ext project 430

---
+++ mcd-chrome-extension/reading-time/scripts/test.js
@@ -0,0 +1,10 @@
+// data-id = wren number
+// td[4] confirmed cases
+
+//loop wren numbers
+//map through wren numbers on excell sheet (price data)
+//multiply confirmed cases with price with matching wren numbers
+//add all prices added to an array together and display
+
+//when data changes on screen run loop
+
