1e818c01b584dd0e2028ba7c5a755f0b5972d256
Claudia McCarter <claudiacole1996@gmail.com>
2024-05-15 14:37:41 -0400

    wed-commit

--- mcd-chrome-extension/reading-time/scripts/test.js
+++ mcd-chrome-extension/reading-time/scripts/test.js
@@ -8,18 +8,47 @@
 
 //when data changes on screen run loop
 
-
-let onloadReady = false;
-
 window.onload = (e) => {
-    console.log('The page is loaded.');
-
-    let truckPriceTotal = 0.00;
-
-    function appendPopUpBox () {
-        const parentContainer = document.getElementsByClassName('page-title')[0];
-        console.log(parentContainer);
-    }
-
-    appendPopUpBox();
+    console.log('The page is loaded');
 }
+
+setTimeout(function() {
+    //visualize when function runs
+    console.log('This is loaded last');
+
+    //modify meta tage to allow script to read jQuery
+    //document.querySelector('meta[name="description"]').setAttribute("content", _desc);
+    
+    /*add jQuery script tag to head of HTML
+    const headID = document.getElementsByTagName("head")[0];         
+    const newScript = document.createElement('script');
+    newScript.type = 'text/javascript';
+    newScript.src = 'jquery-3.4.1.min.js';
+    headID.appendChild(newScript);*/
+
+    //get elements from DOM
+    /*const table = $('#tab-2 table body');
+    const rows = table.getElementsByTagName('TR');*/
+
+    const table = document.querySelector('#tab-2');
+    const rows = table.getElementsByTagName('TR');
+    console.log(table);
+    console.log(rows[1].textContent);
+
+    //empty array for created object data
+    let tableData = [];
+
+    //function to get all rows into custom objects to add price to
+    function appendData () {
+        rows.forEach(function(row) {
+            let object = {};
+            cols = row.getElementsByTagName('TD');
+            object.id = cols[1].textContent;
+            object.qnt = cols[4].textContent;
+        })
+        tableData.push(object);
+    }
+    //call function
+    appendData();
+    console.log(tableData[0]);
+}, 5000);
